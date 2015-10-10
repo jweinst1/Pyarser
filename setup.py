b6663913b5bb9c5d10670953fcf4ae175c161b84
Joshua Weinstein <jweinst1@berkeley.edu>
2015-10-10 14:05:13 -0700

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'Pyrser',
-  packages = ['Tagineer'], # this must be the same as the name above
-  version = '0.1.5',
-  description = 'A Part of Speech Tagging App Written in Python',
+  packages = ['Pyrser'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A Nifty HTML Parser written in Python',
   author = 'Joshua Weinstein',
   author_email = 'jweinst1@berkeley.edu',
-  url = 'https://github.com/jweinst1/Tagineer', # use the URL to the github repo
-  download_url = 'https://github.com/jweinst1/Tagineer/tarball/0.1', # I'll explain this in a second
-  keywords = ['linguistics', 'NLP', 'part of speech'], # arbitrary keywords
+  url = 'https://github.com/jweinst1/Pyrser', # use the URL to the github repo
+  download_url = 'https://github.com/jweinst1/Pyrser/tarball/0.1', # I'll explain this in a second
+  keywords = ['data_science, web, data, harvesting'], # arbitrary keywords
   classifiers = [],
 )
 
