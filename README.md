493f5d62d284a73be4166cf8426897ab3f97320d
Joshua Weinstein <jweinst1@berkeley.edu>
2015-10-13 00:43:03 -0700

    new name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Pyrser
+# Pyarser
 
 An HTML Parser Package written for Python
 
-Pyrser enables users to harvest HTML data in a simple straight forward way, making that data available in a varity of data structures.
+Pyarser enables users to harvest HTML data in a simple straight forward way, making that data available in a varity of data structures.
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(
-  name = 'Pyrser',
-  packages = ['Pyrser'], # this must be the same as the name above
+  name = 'Pyarser',
+  packages = ['Pyarser'], # this must be the same as the name above
   version = '0.1.0',
   description = 'A Nifty HTML Parser written in Python',
-  long_description= """Pyrser is a simple, straight forward HTML parser that allows you to easily harvest text
+  long_description= """Pyarser is a simple, straight forward HTML parser that allows you to easily harvest text
   inside an HTML document from a link to that website. Examples:\n
   get_site_HTML(link): returns a string of HTML content from a link\n
   get_site_text(link): returns a string of text from a link. This string has all the HTML tags <> removed, along
@@ -16,7 +16,7 @@ setup(
   HTML_to_TXT(link, name): writes a TXT file with the text content from a link. All HTML brackets and tags are moved.\n""",
   author = 'Joshua Weinstein',
   author_email = 'jweinst1@berkeley.edu',
-  url = 'https://github.com/jweinst1/Pyrser', # use the URL to the github repo
+  url = 'https://github.com/jweinst1/Pyarser', # use the URL to the github repo
   download_url = 'https://github.com/jweinst1/Pyrser/tarball/0.1', # I'll explain this in a second
   keywords = ['data_science, web, data, harvesting'], # arbitrary keywords
   classifiers = [],
